58b934bfd48f28b04704b339b75879fb800721bf
unknown <ayarabea554@gmail.com>
2020-11-22 20:47:58 +0200

    home page

--- deja-vu/src/app/components/vision/vision.component.ts
+++ deja-vu/src/app/components/vision/vision.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisionComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+  }
+  
 
   ngOnInit(): void {
   }
 
 }
+
+
+
+
+
